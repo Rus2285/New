b926d9924ed65c0fa2567b52751ac220e4d2bddf
Ruslan <rus2285@mail.ru>
2021-05-28 15:57:15 +0300

    leeson #6

---
+++ 222.py
@@ -0,0 +1,24 @@
+
+import my_cred
+from pages import *
+from selenium import webdriver
+from time import sleep
+
+browser = webdriver.Firefox()
+browser.implicitly_wait(5)
+
+home_page = HomePage(browser)
+login_page = home_page.go_to_login_page()
+login_page.login(my_cred.my_login, my_cred.my_pass)
+
+
+#browser.get('https://www.instagram.com/')
+#username_input = browser.find_element_by_css_selector("input[name='username']")
+#password_input = browser.find_element_by_css_selector("input[name='password']")
+
+#username_input.send_keys(my_cred.my_login)
+#password_input.send_keys(my_cred.my_pass)
+#login_button = browser.find_element_by_css_selector("button[type='submit']")
+#login_button.click()
+sleep(15)
+#browser.close()
